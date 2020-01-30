6631d4ea082e9460157b6bbe9bef326efe477e45
Ezy <ezrakeddell@gmail.com>
2020-01-31 12:40:24 +1300

    map added

--- themes/meghna-hugo/assets/js/script.js
+++ themes/meghna-hugo/assets/js/script.js
@@ -146,120 +146,141 @@ jQuery(function ($) {
 	/*	Google Map Customization
 	/* =========================================================================  */
 
+	// function initialize() {
+
+	// 	var latitude = $('#map-canvas').attr('data-latitude');
+	// 	var longitude = $('#map-canvas').attr('data-longitude');
+	// 	var myLatLng = new google.maps.LatLng(latitude, longitude);
+
+	// 	var roadAtlasStyles = [{
+	// 		"featureType": "landscape",
+	// 		"elementType": "geometry.fill",
+	// 		"stylers": [{
+	// 			"color": "#2F3238"
+	// 		}]
+	// 	}, {
+	// 		"elementType": "labels.text.fill",
+	// 		"stylers": [{
+	// 			"color": "#FFFFFF"
+	// 		}]
+	// 	}, {
+	// 		"elementType": "labels.text.stroke",
+	// 		"stylers": [{
+	// 			"visibility": "off"
+	// 		}]
+	// 	}, {
+	// 		"featureType": "road",
+	// 		"elementType": "geometry.fill",
+	// 		"stylers": [{
+	// 			"color": "#50525f"
+	// 		}]
+	// 	}, {
+	// 		"featureType": "road",
+	// 		"elementType": "geometry.stroke",
+	// 		"stylers": [{
+	// 			"visibility": "on"
+	// 		}, {
+	// 			"color": "#808080"
+	// 		}]
+	// 	}, {
+	// 		"featureType": "poi",
+	// 		"elementType": "labels",
+	// 		"stylers": [{
+	// 			"visibility": "off"
+	// 		}]
+	// 	}, {
+	// 		"featureType": "transit",
+	// 		"elementType": "labels.icon",
+	// 		"stylers": [{
+	// 			"visibility": "off"
+	// 		}]
+	// 	}, {
+	// 		"featureType": "poi",
+	// 		"elementType": "geometry",
+	// 		"stylers": [{
+	// 			"color": "#808080"
+	// 		}]
+	// 	}, {
+	// 		"featureType": "water",
+	// 		"elementType": "geometry.fill",
+	// 		"stylers": [{
+	// 			"color": "#3071a7"
+	// 		}, {
+	// 			"saturation": -65
+	// 		}]
+	// 	}, {
+	// 		"featureType": "road",
+	// 		"elementType": "labels.icon",
+	// 		"stylers": [{
+	// 			"visibility": "off"
+	// 		}]
+	// 	}, {
+	// 		"featureType": "landscape",
+	// 		"elementType": "geometry.stroke",
+	// 		"stylers": [{
+	// 			"color": "#bbbbbb"
+	// 		}]
+	// 	}];
+
+	// 	var mapOptions = {
+	// 		zoom: 14,
+	// 		center: myLatLng,
+	// 		disableDefaultUI: true,
+	// 		scrollwheel: false,
+	// 		navigationControl: false,
+	// 		mapTypeControl: false,
+	// 		scaleControl: false,
+	// 		draggable: false,
+	// 		mapTypeControlOptions: {
+	// 			mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'roadatlas']
+	// 		}
+	// 	};
+
+	// 	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+
+	// 	var marker = new google.maps.Marker({
+	// 		position: myLatLng,
+	// 		map: map,
+	// 		title: '',
+	// 	});
+
+
+	// 	google.maps.event.addListener(marker, 'click', function () {
+	// 		infowindow.open(map, marker);
+	// 	});
+
+	// 	var styledMapOptions = {
+	// 		name: 'US Road Atlas'
+	// 	};
+
+	// 	var usRoadMapType = new google.maps.StyledMapType(
+	// 		roadAtlasStyles, styledMapOptions);
+
+	// 	map.mapTypes.set('roadatlas', usRoadMapType);
+	// 	map.setMapTypeId('roadatlas');
+	// }
+
 	function initialize() {
+    var latitude = $("#map-canvas").attr("data-latitude");
+		var longitude = $("#map-canvas").attr("data-longitude");
+    var coords = [latitude, longitude]; // the geographic center of our map
+    var zoomLevel = 3; // the map scale. See: http://wiki.openstreetmap.org/wiki/Zoom_levels
 
-		var latitude = $('#map-canvas').attr('data-latitude');
-		var longitude = $('#map-canvas').attr('data-longitude');
-		var myLatLng = new google.maps.LatLng(latitude, longitude);
-
-		var roadAtlasStyles = [{
-			"featureType": "landscape",
-			"elementType": "geometry.fill",
-			"stylers": [{
-				"color": "#2F3238"
-			}]
-		}, {
-			"elementType": "labels.text.fill",
-			"stylers": [{
-				"color": "#FFFFFF"
-			}]
-		}, {
-			"elementType": "labels.text.stroke",
-			"stylers": [{
-				"visibility": "off"
-			}]
-		}, {
-			"featureType": "road",
-			"elementType": "geometry.fill",
-			"stylers": [{
-				"color": "#50525f"
-			}]
-		}, {
-			"featureType": "road",
-			"elementType": "geometry.stroke",
-			"stylers": [{
-				"visibility": "on"
-			}, {
-				"color": "#808080"
-			}]
-		}, {
-			"featureType": "poi",
-			"elementType": "labels",
-			"stylers": [{
-				"visibility": "off"
-			}]
-		}, {
-			"featureType": "transit",
-			"elementType": "labels.icon",
-			"stylers": [{
-				"visibility": "off"
-			}]
-		}, {
-			"featureType": "poi",
-			"elementType": "geometry",
-			"stylers": [{
-				"color": "#808080"
-			}]
-		}, {
-			"featureType": "water",
-			"elementType": "geometry.fill",
-			"stylers": [{
-				"color": "#3071a7"
-			}, {
-				"saturation": -65
-			}]
-		}, {
-			"featureType": "road",
-			"elementType": "labels.icon",
-			"stylers": [{
-				"visibility": "off"
-			}]
-		}, {
-			"featureType": "landscape",
-			"elementType": "geometry.stroke",
-			"stylers": [{
-				"color": "#bbbbbb"
-			}]
-		}];
-
-		var mapOptions = {
-			zoom: 14,
-			center: myLatLng,
-			disableDefaultUI: true,
-			scrollwheel: false,
-			navigationControl: false,
-			mapTypeControl: false,
-			scaleControl: false,
-			draggable: false,
-			mapTypeControlOptions: {
-				mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'roadatlas']
-			}
-		};
-
-		var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-
-		var marker = new google.maps.Marker({
-			position: myLatLng,
-			map: map,
-			title: '',
-		});
-
-
-		google.maps.event.addListener(marker, 'click', function () {
-			infowindow.open(map, marker);
-		});
-
-		var styledMapOptions = {
-			name: 'US Road Atlas'
-		};
-
-		var usRoadMapType = new google.maps.StyledMapType(
-			roadAtlasStyles, styledMapOptions);
-
-		map.mapTypes.set('roadatlas', usRoadMapType);
-		map.setMapTypeId('roadatlas');
-	}
+    var map = L.map("map-canvas").setView(coords, zoomLevel);
+    var tiles = "http://acetate.geoiq.com/tiles/acetate-hillshading/";
+    L.tileLayer(tiles + "{z}/{x}/{y}.png", {
+      attribution:
+        'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://FortiusOne.com">FortiusOne</a> and <a href="http://stamen.com">Stamen</a>',
+      maxZoom: 18
+		}).addTo(map);
+		  // L.tileLayer("http://tiles.mapc.org/basemap/{z}/{x}/{y}.png", {
+      //   attribution:
+      //     'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
+      //   maxZoom: 17,
+      //   minZoom: 9
+      // }).addTo(map);
+  }
 
 	google.maps.event.addDomListener(window, "load", initialize)
 });
