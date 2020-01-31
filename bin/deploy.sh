2dfe92e53c681bfb4e55ca4684db44709e428d6e
Ezy <ezra@passionfruit.co.nz>
2020-01-31 18:25:46 +1300

    update deploy stamp

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,7 +6,7 @@ rm -rf ../minim-www/*
 mv public/{*,.[^.]*} ../minim-www
 cd ../minim-www
 git add .
-git commit -m "deploy for $(date +%Y%m%d)"
+git commit -m "deploy for $(date +%Y%m%d-%T)"
 git push origin master
 cd ../minim
 rm -rf public
